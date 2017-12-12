52f19fa92b630a5b91749676a2556c8442dc1f6b
Amir Khoury <amirkhourykh@gmail.com>
2017-12-12 14:05:22 +0200

    Created a backend server-code in server.js Reference #5.

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const router = require('./router');
+const http = require('http');
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || 'localhost';
+
+
+const server = http.createServer(router);
+server.listen(port,() => {
+  console.log(`the port is http://${host}:${port}`);
+});
