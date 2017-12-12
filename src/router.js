a214abb4d5694e846f16e20db6111b3a3d8045d4
Ghassan <ghassan.maslamani@gmail.com>
2017-12-12 16:16:09 +0200

    adding the addPost and getPosts functions
    Relates #16

--- src/router.js
+++ src/router.js
@@ -1,21 +1,19 @@
-const handler = require('./handler');
-const addUser = require('./database/addUser');
-const checkUser = require('./database/checkUser');
-const getUser = require ('./database/getUser')
+const handler = require("./handler");
+const addUser = require("./database/addUser");
+const checkUser = require("./database/checkUser");
+const getUser = require("./database/getUser");
+const addPost = require("./database/addPost");
+const getPosts = require("./database/getPosts");
+
 const router = (req, res) => {
   const url = req.url;
-  if (url === '/') {
-    handler(__dirname + '/../index.html','text/html',res);
-    getUser('abchhhef',(result)=>{
-      console.log(result);
-    });
+  if (url === "/") {
+    handler(__dirname + "/../index.html", "text/html", res);
+    getPosts();
   } else {
     res.writeHead(404);
-    res.end('page not found');
+    res.end("page not found");
   }
-}
-
-
-
+};
 
 module.exports = router;
