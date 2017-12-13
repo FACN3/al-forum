5d6772facd424015b4ab726b53ec7f161063b303
SamiSha99 <sami.shakkour@hotmail.com>
2017-12-13 10:56:52 +0200

    Hashing and Process

---
+++ src/hash.js
@@ -0,0 +1,33 @@
+const bcryptjs = require("bcryptjs");
+
+const hashPassword = (password, callback) => {
+  bcrypt.hash(password, 10, (error, hash)  => {
+    if(error){
+      callback(error);
+    } else {
+      callback(null, hash);
+    }
+  });
+};
+
+const comparePasswords = (password, hashedPassword, callback) => {
+  bcrypt.compare(password, hashedPassword, (error, res) => {
+    if(error){
+      callback(error);
+    } else {
+      callback(null, res);
+    }
+  });
+};
+
+
+
+
+
+
+
+
+module.exports = {
+  hashPassword,
+  comparePasswords
+};
